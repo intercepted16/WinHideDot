01feaa0e285d8eb55e791aa89cf8d1a304a462cd
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-05-19 18:12:01 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string userName = Environment.UserName;
+        WatchDirectory($"C:\\Users\\{userName}");
+    }
+
+    private static void WatchDirectory(string directoryPath)
+    {
+        using (var watcher = new FileSystemWatcher(directoryPath))
+        {
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.Created += OnFileCreated;
+            watcher.Renamed += OnFileRenamed;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Watching for file/folder changes...");
+            Console.WriteLine("Press 'q' to quit.");
+
+            while (Console.Read() != 'q') ;
+        }
+    }
+
+    private static void OnFileCreated(object sender, FileSystemEventArgs e)
+    {
+        Console.WriteLine($"File/Folder Added: {e.FullPath}");
+        Check(e.FullPath);
+    }
+    private static void OnFileRenamed(object sender, RenamedEventArgs e)
+    {
+        Console.WriteLine($"File/Folder Renamed: {e.FullPath}");
+        Check(e.FullPath);
+    }
+
+    private static void Check(string filename)
+    {
+        string name = Path.GetFileName(filename);
+        if (name.StartsWith('.') && !File.GetAttributes(filename).HasFlag(FileAttributes.Hidden))
+        {
+            File.SetAttributes(filename, File.GetAttributes(filename) | FileAttributes.Hidden);
+        }
+    }
+}
